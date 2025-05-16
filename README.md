76030941ce4635243d59d44790f827ad1e16d58a
Mira  Sohn <sohnm@helix.nih.gov>
2025-05-16 15:02:03 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # pseudobulk-demo
+
+This workflow demonstrates pseudobulk differential testing (DE) analysis of single cell datasets,
+clustered and annotated using [Scanpy](https://scanpy.readthedocs.io/en/stable/), as demonstrated
+in https://github.com/Mira0507/scanpy-demo. Note that the single cell data was stored both in
+Scanpy's [AnnData](https://anndata.readthedocs.io/en/stable/) and Seurat object for flexible 
+analysis and data visualization. However, the current workflow is designed to use a Seurat object
+as input. 
+
+## Repository structure
+
+```
+$ tree
+.
+├── README.md
+└── scripts
+    ├── helpers.R
+    └── pseudobulk_de.Rmd
+
+```
+
+
---
+++ scripts/helpers.R
@@ -0,0 +1,58 @@
+#' Print Markdown (frequently used to programatically print headers).
+#' Handles header levels and additional newlines. Make sure you're in a chunk
+#' with results='asis' for this to work.
+#'
+#' @param level Markdown header level. 1="#", 2="##", etc
+#' @param ... Arguments passed directly to cat
+#' @return NULL; as a side effect the text is printed.
+
+mdcat <- function(..., level=NA){
+  dots <- paste(...)
+  if (!is.na(level)){
+    header <- paste0(rep('#', level), collapse="")
+    dots <- paste(header, dots)
+  }
+  cat(paste0('\n\n', dots, '\n\n'), fill=1500)
+}
+
+
+# Define a function generating subchunks with DT::datatable(dataframe) each
+subchunkify <- function(name, input='df', width=7, height=5, ggplotly=TRUE) {
+    if (input == 'df') {
+        t_deparsed <- paste0("DT::datatable(df)")
+        more <- ""
+    } else if (input == 'plot') {
+        t_deparsed <- paste0("plotly::ggplotly(p)")
+        if (!ggplotly) {
+            t_deparsed <- paste0("print(p)")
+        }
+        more <- paste0(", fig.width=", width, ", fig.height=", height)
+    } else {
+        stop('Incorrect argument: input')
+    }
+    sub_chunk <- paste0("```{r sub_chunk_", name, ", results='asis', echo=FALSE", more, "}",
+        "\n",
+        t_deparsed,
+        "\n\n```\n\n\n")
+
+    cat(knitr::knit(text = sub_chunk, quiet=TRUE))
+}
+
+
+#' Print a Markdown link for a plot
+#'
+#' @param fn filepath
+#' @return String ready to be printed
+link.plot <- function(fn){
+    cat(paste('\n\n- Download Plot: [', fn, '](', fn, ')\n\n'))
+}
+
+#' Print a Markdown link for a table
+#'
+#' @param fn filepath
+#' @return String ready to be printed
+link.table <- function(fn){
+    cat(paste('\n\n- Download Table: [', fn, '](', fn, ')\n\n'))
+}
+
+
